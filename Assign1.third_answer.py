e550782bb87fcf226657f41403dd6ca79562df28
SurajMadankar <127080716+SurajMadankar@users.noreply.github.com>
2023-03-07 19:19:47 +0530

    Add files via upload

---
+++ Assign1.third_answer.py
@@ -0,0 +1,41 @@
+numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9)  
+
+count_odd = 0
+
+count_even = 0
+
+for x in numbers:
+
+ if  not x % 2:
+  count_even+=1
+ elif  x %2:
+   count_odd+=1 
+
+else:
+
+    "hello"
+
+print("Number of even numbers :",count_even)
+
+print("Number of odd numbers :",count_odd)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
